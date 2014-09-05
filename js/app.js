4555f1f291f38fdbaaca269f1a738c1f8c7c2ad4
C. Robert Dudley <rdudley@rdudleyimacbook.attlocal.net>
2014-09-04 22:46:41 -0500

    add values to db and log to console (rdudleyiii)

--- js/app.js
+++ js/app.js
@@ -1,16 +1,37 @@
 (function() {
 
   // 'global' variable to store reference to the database
-  var db;
+  var db, input;
 
   databaseOpen(function() {
-    console.log("The database has been opened");
+    input = document.querySelector('input');
+
+    document.body.addEventListener('submit', onSubmit);
+    
+    databaseTodosGet(function (todos) {
+      console.log(todos);
+    });
   });
 
+  function onSubmit (e) {
+    e.preventDefault();
+    databaseTodosAdd(input.value, function () {
+      input.value = '';
+    });
+  }
+
   function databaseOpen(callback) {
-    // Open a database, specify the name and version
+   // Open a database, specify the name and version
+    var dbName = 'todos';
     var version = 1;
-    var request = indexedDB.open('todos', version);
+    var request = indexedDB.open(dbName, version);
+
+    // Run migrations if necessary
+    request.onupgradeneeded = function(e) {
+      db = e.target.result;
+      e.target.transaction.onerror = databaseError;
+      db.createObjectStore('todo', { keyPath: 'timeStamp' });
+    };
 
     request.onsuccess = function(e) {
       db = e.target.result;
@@ -21,6 +42,43 @@
 
   function databaseError(e) {
     console.error('An IndexedDB error has occurred', e);
+  }
+
+  function databaseTodosAdd (text, callback) {
+    var trans = db.transaction(['todo'], 'readwrite');
+    var store = trans.objectStore('todo');
+    var req = store.put({
+      text : text,
+      timeStamp : Date.now()
+    });
+
+    trans.oncomplete = function (e) {
+      callback();
+    };
+
+    req.onerror = databaseError;
+  }
+
+  function databaseTodosGet (callback) {
+    var trans = db.transaction(['todo'], 'readonly');
+    var store = trans.objectStore('todo');
+
+    // get stuff from the store
+    var keyRange = IDBKeyRange.lowerBound(0);
+    var cursorRequest = store.openCursor(keyRange);
+
+    var data = [];
+    
+    cursorRequest.onsuccess = function (e) {
+      var result = e.target.result;
+
+      if (result) {
+        data.push(result.value);
+        result.continue();
+      } else {
+        callback(data);
+      }
+    };
   }
 
 }());
